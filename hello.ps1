f368af20cda13fe83882d21b3f4bc2cd32d61897
Lawrence <143608054+llawrencee@users.noreply.github.com>
2025-06-12 11:21:18 +0800

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,3 +1,13 @@
 Write-Host 'Hello from PowerShell!' -ForegroundColor Green
-Start-Sleep -Seconds 10
+Write-Host '5'
+Start-Sleep -Seconds 1
+Write-Host '4'
+Start-Sleep -Seconds 1
+Write-Host '4'
+Start-Sleep -Seconds 1
+Write-Host '3'
+Start-Sleep -Seconds 1
+Write-Host '2'
+Start-Sleep -Seconds 1
+Write-Host '1'
 Write-Host 'Text after 10 seconds.'
